f155440be4a2833b7e5586a9ff514bb1450de9b2
Brett Whittaker <brett.whittaker@forces.gc.ca>
2025-04-23 16:49:42 -0400

    save changes

---
+++ src/ca.whittaker.Maui.Controls/BoolToButtonStateEnumConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ca.whittaker.Maui.Controls;
+
+public class BoolToButtonStateEnumConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool isLive)
+        {
+            return isLive ? ButtonStateEnum.Enabled : ButtonStateEnum.Disabled; // Adjust these enum values based on your ButtonStateEnum
+        }
+        return ButtonStateEnum.Enabled; // Or your default enum state
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/ca.whittaker.Maui.Controls/Platforms/Tizen/PlatformClass1.cs
+++ src/ca.whittaker.Maui.Controls/Platforms/Tizen/PlatformClass1.cs
@@ -3,7 +3,7 @@
 namespace ca.whittaker.Maui.Controls
 {
     // All the code in this file is only included on Tizen.
-    public class PlatformClass1
+    public partial class PlatformClass1
     {
     }
 }
