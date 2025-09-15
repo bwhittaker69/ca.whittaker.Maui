e293e4bf8ed8f48718b83f83b5ee4ab4519691d5
Brett Whittaker <brett.whittaker@ecn.forces.gc.ca>
2025-09-15 17:21:59 -0400

    updates

--- src/ca.whittaker.Maui.Controls/DeviceHelper.cs
+++ src/ca.whittaker.Maui.Controls/DeviceHelper.cs
@@ -7,7 +7,7 @@ public static class DeviceHelper
     {
         double density = DeviceDisplay.MainDisplayInfo.Density + ((double)scaleFactor / 10);
         if (density <= 1) return 12; // mdpi
-        if (density <= 1.5) return 12; // hdpi
+        if (density <= 1.5) return 24; // hdpi
         if (density <= 2) return 24; // xhdpi
         if (density <= 3) return 24; // xxhdpi
         return 48; // xxxhdpi or higher
